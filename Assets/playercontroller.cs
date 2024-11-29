9e5ca7abdf83aae2c91c552f94b5ec6387a501b0
pstrismus <97733906+pstirmus@users.noreply.github.com>
2024-11-29 23:29:43 +0300

    .

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    [SerializeField]
+    private float haraketHizi;
+    [SerializeField]
+    private float ziplamaGucu;
+    private SpriteRenderer sRenderer;
+    Rigidbody2D rb;
+    Animator anim;
+    int yon;
+    bool ciftzıplayabilir;
+
+
+
+    
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        sRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(yon * haraketHizi * Time.deltaTime,0,0);
+        if (yon!=0)
+        {
+            anim.SetBool("kosuyor", true);
+
+            if (yon > 0)
+            {
+                sRenderer.flipX = false;
+            }
+            else if (yon < 0)
+            {
+                sRenderer.flipX = true;
+            }
+
+        }
+        else
+        {
+            anim.SetBool("kosuyor", false);
+        }
+       // sagsol();
+        
+    }
+    
+
+    public bool YerdeMi()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.6f, 1 << LayerMask.NameToLayer("Zemin"));
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    public void Yonkarar(int yon)
+    {
+        this.yon = yon;
+    }
+    public void jump()
+    {
+        if (ciftzıplayabilir == false)
+        {
+            if (YerdeMi())
+            {
+                Vector2 zıplamaVektor = new Vector2(0, 1) * ziplamaGucu;
+                rb.AddForce(zıplamaVektor);
+                //rb.AddForce(Vector2.up*ziplamaGucu); 
+                ciftzıplayabilir = true;
+                
+                
+            }
+        }
+        else if (ciftzıplayabilir == true)
+        {
+            Vector2 zıplamaVektor = new Vector2(0, 1) * ziplamaGucu;
+            rb.AddForce(zıplamaVektor);
+            ciftzıplayabilir = false;
+        }
+
+    }
+}
---
+++ Assets/skils.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skils : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //dash
+    [Header("dash")]
+    public float dashAmount = 7f;
+    public float dashtime = 0.1f;
+    public float HorizontalMove = 7f;
+    public bool canDash = true;
+    public bool isDashing;
+    public float dashColdown;
+    private SpriteRenderer sRenderer;
+    [Header("mutation")]
+    
+    public Vector3 mainscharactertransform;
+    public GameObject maincharacterscale;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        mainscharactertransform = gameObject.transform.position;
+        sRenderer = gameObject.GetComponent<SpriteRenderer>();
+        Debug.Log(Input.touchCount);
+    }
+
+
+    public void dash()
+    {
+        StartCoroutine(dashh());
+
+    }
+
+    IEnumerator dashh()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.gravityScale = 0;
+        if (!sRenderer.flipX)
+        {
+            rb.velocity = new Vector2(HorizontalMove * dashAmount, 1f);
+        }
+        else if (sRenderer.flipX)
+        {
+            rb.velocity = new Vector2(-50 - -1.3f , 1f);
+        }
+        yield return new WaitForSeconds(dashtime);
+        rb.velocity = new Vector2(0f, 0f);
+        rb.gravityScale = 1f;
+        isDashing = false;
+        yield return new WaitForSeconds(dashColdown);
+        canDash = true;
+        
+    }
+ 
+
+}
