cffb9dd2d7e4f576f43e3b57fb65b5c28a3f453a
Kaan SARI <137452107+atifkaansari@users.noreply.github.com>
2024-11-30 17:11:46 +0300

    00

---
+++ Assets/script/exitgame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitgame : MonoBehaviour
+{
+   void QuitGame () {
+Application.Quit ();
+Debug.Log("Game is exiting");
+}
+    
+}
